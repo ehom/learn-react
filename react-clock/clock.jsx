fbef2def73000d710f40a0945c0338ac20d6b4fb
ehom <ehom@users.noreply.github.com>
2018-12-31 16:50:36 -0800

    Update clock.jsx

--- react-clock/clock.jsx
+++ react-clock/clock.jsx
@@ -24,7 +24,7 @@ class Clock extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, world !!!</h1>
+        <h1>Hello, world.</h1>
         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
